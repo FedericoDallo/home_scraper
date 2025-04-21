41f89f2673a78e2cc5e487a0a6772e51897130f9
Fede <fededallo1703@gmail.com>
2025-04-20 23:56:10 -0300

    Increased max price on MercadoLibre while garage scrape funcionality is a wip

--- app/services/mercado_libre_scraper.rb
+++ app/services/mercado_libre_scraper.rb
@@ -17,8 +17,8 @@ class MercadoLibreScraper < BaseScraper
 
   def listings_path
     [
-      "/mas-de-1-dormitorios/_PublishedToday_YES_PriceRange_0UYU-35000UYU_COVERED*AREA_45-*_FULL*BATHROOMS_1-*_PROPERTY*TYPE_242062,242060_item*location_lat:-34.918204*-34.906844,lon:-56.152747*-56.143262",
-      "/mas-de-1-dormitorios/_PriceRange_0UYU-35000UYU_PublishedToday_YES_COVERED*AREA_50-*_PROPERTY*TYPE_242060,242062_TOTAL*AREA_55-*_item*location_lat:-34.926307*-34.908212,lon:-56.162405*-56.147295"
+      "/mas-de-1-dormitorios/_PublishedToday_YES_PriceRange_0UYU-#{MAX_PRICE + GARAGE_PRICE_INCREASE}UYU_COVERED*AREA_45-*_FULL*BATHROOMS_1-*_PROPERTY*TYPE_242062,242060_item*location_lat:-34.918204*-34.906844,lon:-56.152747*-56.143262",
+      "/mas-de-1-dormitorios/_PriceRange_0UYU-#{MAX_PRICE + GARAGE_PRICE_INCREASE}UYU_PublishedToday_YES_COVERED*AREA_50-*_PROPERTY*TYPE_242060,242062_TOTAL*AREA_55-*_item*location_lat:-34.926307*-34.908212,lon:-56.162405*-56.147295"
     ]
   end
 
